30a42e073b253c3e5e8867d2d813186dade3671c
Pedro Torres <pebtorres@hotmail.com>
2024-09-14 14:38:06 -0400

    feat: middleware for entries validation - done

--- src/routers/prayers.routers.ts
+++ src/routers/prayers.routers.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import prayersController from "../controllers/prayers.controller";
+import validateEntries from "../middlewares/entries.middleware";
 
 const prayersRouter = Router();
 
 prayersRouter.get('/prayers', prayersController.list);
 
-prayersRouter.post('/create', prayersController.create);
+prayersRouter.post('/prayers', validateEntries, prayersController.create);
 
 export default prayersRouter;
