307ecb7d058d6222a6ada130a3ed1a28303fc486
Pedro Torres <pebtorres@hotmail.com>
2024-09-16 12:39:37 -0400

    feat: login route working

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import prayersRouter from '../routers/prayers.routers';
+import loginRouter from '../routers/login.routers';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(prayersRouter);
+
+app.use(loginRouter);
 
 export default app;
